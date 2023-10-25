0fbea8cd31e7c7aceba1eef448edfd71b0592858
Bob Guo <bobguo1234@gmail.com>
2023-10-26 04:34:18 +1100

    Your new commit message here

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function processCSV() {
+    // Make an XMLHttpRequest to fetch the CSV file
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "data/sales.csv", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Split the CSV content into rows
+            var rows = xhr.responseText.split('\n');
+            // console.log(rows)
+            // Extract the ID from the first row
+            var id_from_csv = rows[1].split(',');
+            var id = id_from_csv[0]
+
+            // Generate the dynamic URL
+            var dynamic_url = `https://assets.axieinfinity.com/axies/${id}/axie/axie-full-transparent.png`;
+
+            // Get the image element by its id
+            var imgElement = document.getElementById("dynamicImage");
+
+            // Update the image source
+            imgElement.src = dynamic_url;
+        }
+    };
+    xhr.send();
+}
+
+// Call the processCSV function to load the dynamic image
+processCSV();
+
+
+
 // Function to toggle column visibility
 function toggleColumnVisibility(tableId, column) {
     const table = document.getElementById(tableId);
@@ -31,6 +62,7 @@ function displayCSVData(file, tableId) {
             const rows = data.split('\n');
             const headers = rows[0].split(',');
             const fragment = document.createDocumentFragment();
+            let firstId = null; // Variable to store the first ID
 
             for (let i = 0; i < rows.length; i++) {
                 const cells = rows[i].split(',');
@@ -39,11 +71,17 @@ function displayCSVData(file, tableId) {
                 for (let j = 0; j < headers.length; j++) {
                     const cell = (i === 0) ? document.createElement('th') : document.createElement('td');
                     cell.textContent = cells[j];
+                    if (i === 0 && headers[j] === "ID") {
+                        firstId = cells[j];
+                    }
                     row.appendChild(cell);
                 }
                 fragment.appendChild(row);
             }
             table.appendChild(fragment);
+            if (firstId) {
+                addFirstIdToTables(firstId);
+            }
         });
 }
 
@@ -62,12 +100,10 @@ function addImageToTables(imageUrl) {
     const tableContainers = document.querySelectorAll('.table-container');
     const tableHeight = document.getElementById('virgin-table').clientHeight; // Get the height of one of the tables (assuming they have the same height)
 
-    image.style.height = tableHeight + 'px'; // Set the image's height to match the table's height
+    image.style.height = (tableHeight*0.5) + 'px'; // Set the image's height to match the table's height
+    image.style.width = 'auto';
 
     tableContainers.forEach(container => {
         container.insertBefore(image.cloneNode(true), container.firstChild);
     });
 }
-
-// Add a single image to the left of all tables with the same height as the tables
-addImageToTables('https://assets.axieinfinity.com/axies/123123/axie/axie-full-transparent.png');
