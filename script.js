8961bcf41bc9f9895289ac8eb439986b96f47fee
Bob Guo <bobguo1234@gmail.com>
2023-10-25 23:45:41 +1100

    Your new commit message here

--- script.js
+++ script.js
@@ -22,31 +22,52 @@ columnButtons.forEach(button => {
     });
 });
 
-// Sample data loading (You should replace this with your data loading logic)
+// Function to display CSV data in a table
 function displayCSVData(file, tableId) {
     fetch(`data/${file}`)
         .then(response => response.text())
         .then(data => {
             const table = document.getElementById(tableId);
             const rows = data.split('\n');
-            const headerRow = table.insertRow();
             const headers = rows[0].split(',');
-            headers.forEach(header => {
-                const headerCell = headerRow.insertCell();
-                headerCell.textContent = header;
-            });
-            for (let i = 1; i < rows.length; i++) {
-                const dataRow = table.insertRow();
+            const fragment = document.createDocumentFragment();
+
+            for (let i = 0; i < rows.length; i++) {
                 const cells = rows[i].split(',');
-                cells.forEach(cell => {
-                    const dataCell = dataRow.insertCell();
-                    dataCell.textContent = cell;
-                });
+                const row = document.createElement('tr');
+
+                for (let j = 0; j < headers.length; j++) {
+                    const cell = (i === 0) ? document.createElement('th') : document.createElement('td');
+                    cell.textContent = cells[j];
+                    row.appendChild(cell);
+                }
+                fragment.appendChild(row);
             }
+            table.appendChild(fragment);
         });
 }
 
+// Load CSV data into tables
 displayCSVData('virgin.csv', 'virgin-table');
 displayCSVData('bred.csv', 'bred-table');
 displayCSVData('floor.csv', 'floor-table');
 displayCSVData('sales.csv', 'sales-table');
+
+// Function to add an image to the left of all tables
+function addImageToTables(imageUrl) {
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    image.alt = 'Table Image'; // Add an alt text for accessibility
+
+    const tableContainers = document.querySelectorAll('.table-container');
+    const tableHeight = document.getElementById('virgin-table').clientHeight; // Get the height of one of the tables (assuming they have the same height)
+
+    image.style.height = tableHeight + 'px'; // Set the image's height to match the table's height
+
+    tableContainers.forEach(container => {
+        container.insertBefore(image.cloneNode(true), container.firstChild);
+    });
+}
+
+// Add a single image to the left of all tables with the same height as the tables
+addImageToTables('https://assets.axieinfinity.com/axies/123123/axie/axie-full-transparent.png');
