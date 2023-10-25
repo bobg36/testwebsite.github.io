6d588b87fde738a5f0c90cee86edb7e2a24e3f36
Bob Guo <bobguo1234@gmail.com>
2023-10-25 19:53:37 +1100

    Your commit message here

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // List of CSV files you want to load
-    const csvFiles = ["data/data1.csv", "data/data2.csv"];
+    const csvFiles = ["data/data1.csv", "data/data2.csv", "data/data3.csv", "data/data4.csv"];
 
     // Function to load and parse CSV files
     function loadCSVData(file) {
@@ -11,28 +11,41 @@ document.addEventListener("DOMContentLoaded", function () {
                     header: true,
                     dynamicTyping: true,
                     complete: function (results) {
-                        displayData(results.data);
+                        displayData(results.data, file);
                     }
                 });
             });
     }
 
     // Function to display data on the page
-    function displayData(data) {
+    function displayData(data, fileName) {
         const dataContainer = document.getElementById("data-container");
 
-        data.forEach(item => {
-            const itemDiv = document.createElement("div");
-            itemDiv.classList.add("data-item");
+        // Create a container for the data from each file
+        const fileContainer = document.createElement("div");
+        fileContainer.classList.add("file-container");
+
+        const fileTitle = document.createElement("h2");
+        fileTitle.textContent = `Data from ${fileName}`;
+
+        fileContainer.appendChild(fileTitle);
+
+        // Display the first 5 rows of data
+        for (let i = 0; i < Math.min(5, data.length); i++) {
+            const row = data[i];
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("data-row");
 
             // Customize how you display each item from the CSV here
-            itemDiv.innerHTML = `
-                <h2>${item.name}</h2>
-                <p>${item.description}</p>
+            rowDiv.innerHTML = `
+                <h3>${row.name}</h3>
+                <p>${row.description}</p>
             `;
 
-            dataContainer.appendChild(itemDiv);
-        });
+            fileContainer.appendChild(rowDiv);
+        }
+
+        dataContainer.appendChild(fileContainer);
     }
 
     // Load data from CSV files
