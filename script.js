64d221c62a4f1f316a763f42fef7619005529b9e
Bob Guo <bobguo1234@gmail.com>
2023-10-25 22:20:46 +1100

    Your new commit message here

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-// Function to display CSV data
+// Function to toggle column visibility
+function toggleColumnVisibility(tableId, column) {
+    const table = document.getElementById(tableId);
+    const columnIndex = Array.from(table.rows[0].cells).findIndex(cell => cell.textContent === column);
+
+    if (columnIndex >= 0) {
+        const rows = table.rows;
+        for (let i = 0; i < rows.length; i++) {
+            const cell = rows[i].cells[columnIndex];
+            cell.style.display = cell.style.display === 'none' ? '' : 'none';
+        }
+    }
+}
+
+// Add event listeners to the buttons
+const columnButtons = document.querySelectorAll('.column-buttons button');
+columnButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const tableId = button.parentElement.getAttribute('data-table');
+        const column = button.getAttribute('data-column');
+        toggleColumnVisibility(tableId, column);
+    });
+});
+
+// Sample data loading (You should replace this with your data loading logic)
 function displayCSVData(file, tableId) {
     fetch(`data/${file}`)
         .then(response => response.text())
         .then(data => {
             const table = document.getElementById(tableId);
-
-            // Split the CSV data into rows
             const rows = data.split('\n');
-
-            // Display the header row
             const headerRow = table.insertRow();
             const headers = rows[0].split(',');
             headers.forEach(header => {
                 const headerCell = headerRow.insertCell();
                 headerCell.textContent = header;
             });
-
-            // Display the first 5 rows of data
-            for (let i = 1; i < 6 && i < rows.length; i++) {
+            for (let i = 1; i < rows.length; i++) {
                 const dataRow = table.insertRow();
                 const cells = rows[i].split(',');
                 cells.forEach(cell => {
@@ -28,8 +46,5 @@ function displayCSVData(file, tableId) {
         });
 }
 
-// Display data from data1.csv
 displayCSVData('data1.csv', 'data1-table');
-
-// Display data from data2.csv
 displayCSVData('data2.csv', 'data2-table');
